4553448b9701d974b9ae57806034e4f9313439d5
RuslanKhamitov19 <rus.khamitov.19@bk.ru>
2025-08-27 12:01:51 +0500

    lesson03

--- 03_lesson/lesson_3_task_3.py
+++ 03_lesson/lesson_3_task_3.py
@@ -5,6 +5,6 @@ to_address = Address("148734", "Париж", "Ленина", "123", "34")
 
 from_address = Address("456728", "Краснодар", "Набережная", "12", "111")
 
-MAiling = Mailing("75463597", from_address, to_address, "1299")
+MAiling = Mailing("75463597", to_address, from_address, "1299")
 
 print(MAiling)
