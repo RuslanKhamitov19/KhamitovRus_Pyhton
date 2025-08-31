25bb387c1510739390ccb154a95a5d4d16bb4db4
RuslanKhamitov19 <rus.khamitov.19@bk.ru>
2025-08-31 14:12:13 +0500

    lesson4

--- 04_lesson/test_string_utils.py
+++ 04_lesson/test_string_utils.py
@@ -25,6 +25,37 @@ def test_capitalize_negative(input_str, expected):
     assert string_utils.capitalize(input_str) == expected
 
 
+@pytest.mark.positive
+@pytest.mark.parametrize("input_string, expected", [
+    ("  skypro", "skypro"),
+])
+def test_trim_positive(input_string, expected):
+    assert string_utils.trim(input_string) == expected
+
+
+@pytest.mark.negative
+@pytest.mark.parametrize("input_string, expected", [
+    ("skypro", "skypro"),
+])
+def test_trim_negative(input_string, expected):
+    assert string_utils.trim(input_string) == expected
+
+
+@pytest.mark.positive
+@pytest.mark.parametrize("Input_string, symbol", [
+    ("SkyPro", "S"),
+])
+def test_contains_positive(Input_string, symbol,):
+    assert string_utils.contains(Input_string, symbol)
+
+
+@pytest.mark.negative
+@pytest.mark.parametrize("Input_string, symbol", [
+    ("SkyPro", "U"),
+])
+def test_contains_negative(Input_string, symbol):
+    assert not string_utils.contains(Input_string, symbol)
+
 
 
 @pytest.mark.positive
@@ -37,7 +68,7 @@ def test_delete_symbol_positive(string, symbol, expected_result):
 
 @pytest.mark.negative
 @pytest.mark.parametrize("string, symbol, expected_result", [
-    ("SkyPro", "S", "SkyPro"),
+    ("SkyPro", "W", "SkyPro"),
 ])
 def test_delete_symbol_positive(string, symbol, expected_result):
     assert string_utils.delete_symbol(string, symbol) == expected_result
